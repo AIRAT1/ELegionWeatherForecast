cd21e7e556ec57d67a29ed2b3bec0bbb70d9f30e
Ayrat <ayrat1@mail.ru>
2016-06-17 23:43:33 +0300

    Initial commit

---
+++ app/src/main/java/de/android/elegionweatherforecast/MainActivity.java
@@ -0,0 +1,85 @@
+package de.android.elegionweatherforecast;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayAdapter<String> forecastAdapter;
+    private ListView listView;
+    String[] data = {
+            "Berlin",
+            "Muenchen",
+            "Hamburg",
+            "Koeln",
+            "Duesseldorf"
+    };
+    List<String> forecast = new ArrayList<>(Arrays.asList(data));
+
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        forecastAdapter = new ArrayAdapter<>(
+                this,
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_text_view,
+                forecast);
+        listView = (ListView)findViewById(R.id.list_view_forecast);
+        listView.setAdapter(forecastAdapter);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, R.string.add_a_new_city, Snackbar.LENGTH_LONG)
+                        .setAction(R.string.yes, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this, "Add new city here", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
