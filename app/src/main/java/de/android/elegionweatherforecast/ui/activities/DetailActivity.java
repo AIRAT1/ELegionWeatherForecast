46c894cdcd7cfaa13712c54f95c05ab6a6704a66
AIRAT1 <ayrat1@mail.ru>
2016-06-22 17:25:08 +0400

    add DetailActivity with MainActivity as a parent

---
+++ app/src/main/java/de/android/elegionweatherforecast/ui/activities/DetailActivity.java
@@ -0,0 +1,32 @@
+package de.android.elegionweatherforecast.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import de.android.elegionweatherforecast.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
