ef2272350477b4a3bccaeff4884cbb8d8ed26146
Ayrat <ayrat1@mail.ru>
2016-07-03 10:47:12 +0300

    beginning to add db

---
+++ app/src/main/java/de/android/elegionweatherforecast/data/WeatherContract.java
@@ -0,0 +1,12 @@
+package de.android.elegionweatherforecast.data;
+
+import android.text.format.Time;
+
+public class WeatherContract {
+    public static long normalizeDate(long startDate) {
+        Time time = new Time();
+        time.set(startDate);
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
+    }
+}
