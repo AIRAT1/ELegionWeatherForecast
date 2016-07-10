512766ffccb04e7e55949f4664c46d2b07dcf695
AIRAT1 <ayrat1@mail.ru>
2016-07-10 18:44:58 +0400

    add more details in DetailActivity

--- app/src/main/java/de/android/elegionweatherforecast/ui/activities/DetailActivity.java
+++ app/src/main/java/de/android/elegionweatherforecast/ui/activities/DetailActivity.java
@@ -17,7 +17,34 @@ public class DetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
             String forecstString = intent.getStringExtra(Intent.EXTRA_TEXT);
-            ((TextView)findViewById(R.id.detail_text)).setText(forecstString);
+
+            String[] forecasts = forecstString.split(" ");
+
+            if (forecasts.length == 11) {
+                ((TextView)findViewById(R.id.detail_city_name)).setText("City: " + forecasts[0]);
+                ((TextView)findViewById(R.id.detail_temperature)).setText("Current temperature: " + forecasts[1]);
+                ((TextView)findViewById(R.id.detail_date)).setText("Current date: " + forecasts[2] +
+                        " " + forecasts[3] + " " + forecasts[4]);
+                ((TextView)findViewById(R.id.detail_weather)).setText("Current weather: " +
+                        forecasts[5] + " " + forecasts[6]);
+                ((TextView)findViewById(R.id.detail_wind_speed)).setText("Wind speed: " + forecasts[7]);
+                ((TextView)findViewById(R.id.detail_wind_degree)).setText("Wind direction: " + getWindDegreeDescription(forecasts[8]));
+                ((TextView)findViewById(R.id.detail_pressure)).setText("Pressure: " + forecasts[9]);
+                ((TextView)findViewById(R.id.detail_max_min_temperature)).setText("max/min day temperature: " + forecasts[10]);
+            }
         }
+    }
+
+    private String getWindDegreeDescription(String forecast) {
+        double degree = Double.parseDouble(forecast);
+        if (degree >= 339 || degree < 23) return "North";
+        if (degree >= 23 || degree < 68) return "North - East";
+        if (degree >= 68 || degree < 113) return "East";
+        if (degree >= 113 || degree < 158) return "South - East";
+        if (degree >= 158 || degree < 203) return "South";
+        if (degree >= 203 || degree < 248) return "South - West";
+        if (degree >= 248 || degree < 293) return "West";
+        if (degree >= 293 || degree < 339) return "North - West";
+        return null;
     }
 }
